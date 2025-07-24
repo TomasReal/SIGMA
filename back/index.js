624561557480451a723154de25837d97b38b1591
TomasReal <vrtomas1@gmail.com>
2025-07-24 20:27:13 -0300

    remove garbage code

--- back/index.js
+++ back/index.js
@@ -68,6 +68,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 // Necesario para __dirname en ESModules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -76,8 +77,12 @@ app.use(cors());
 app.use(express.json());
 
 // 👉 Servir el frontend
+// app.use(express.static(path.join(__dirname, 'public', 'dist')));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'dist', 'index.html'));
+// });
 app.use(express.static(path.join(__dirname, 'public', 'dist')));
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'dist', 'index.html'));
 });
 
